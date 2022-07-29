bf4e9b214bf1b59b85d7b24f4837058cbd4d99db
gitthabet <tchelligue@gmail.com>
2022-07-29 23:42:02 +0400

    edit main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from BankAccount import CheckingAccount
 
-chechacc = CheckingAccount(1000, 5)
 
-chechacc.deposit(500)
-print(chechacc.balance)
 
-chechacc.withdraw(500, 50)
-print(chechacc.balance)
+
+if __name__=="__main__":
+    chechacc = CheckingAccount(1000, 5)
+    chechacc.deposit(500)
+    print(chechacc.balance)
+    chechacc.withdraw(500, 50)
+    print(chechacc.balance)
