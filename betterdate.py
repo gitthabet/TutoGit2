b771344a09fc119abf44adca18050956b1740b70
gitthabet <tchelligue@gmail.com>
2022-07-29 23:26:35 +0400

    add betterdate

---
+++ betterdate.py
@@ -0,0 +1,17 @@
+class BetterDate:    
+    # Constructor
+    def __init__(self, year, month, day):
+      # Recall that Python allows multiple variable assignments in one line
+      self.year, self.month, self.day = year, month, day
+    
+    
+    
+    # Define a class method from_str
+    @classmethod
+    def from_str(cls, datestr):
+        # Split the string at "-" and convert each part to integer
+        #parts = datestr.split("-")
+        year, month, day = map(int, datestr.split("-"))
+        #year, month, day = int(parts[0]), int(parts[1]), int(parts[2])
+        # Return the class instance
+        return BetterDate(year, month, day)
