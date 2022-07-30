bbcd3296de681280092b265f7c957d028f043240
gitthabet <tchelligue@gmail.com>
2022-07-30 13:40:36 +0400

    commit delete pyc

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+'''from BankAccount import CheckingAccount
+
+
+if __name__=="__main__":
+    chechacc = CheckingAccount(1000, 5)
+    chechacc.deposit(500)
+    print(chechacc.balance)
+    chechacc.withdraw(500, 50)
+    print(chechacc.balance)
+
+'''
 # from BankAccount import CheckingAccount
 
 from BetterDate import BetterDate
